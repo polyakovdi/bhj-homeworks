1956c8a034de88dee362d6644efb1d5710393401
polyakovdi <goodboy393393@gmail.com>
2023-05-02 19:28:30 +0300

    First commit

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,8 +1,15 @@
 //Задание 2
-const cookieElem = document.getElementById('cookie');
-const cookieClicker = document.getElementById('clicker__counter');
-let counter = 0;
+let image = document.getElementById("cookie");
+let count = document.getElementById("clicker__counter");
 
-cookieElem.onclick = () => {
-  cookieElem.width = ++cookieClicker.textContent % 2 ? 250 : 200
+function clickFunction() {
+  let clickCount = +count.textContent;
+  count.textContent = clickCount + 1;
+  if (clickCount % 2 == 0 ) {
+    image.width = 250;
+  } else {
+    image.width = 200;
+  }
 }
+
+image.onclick = clickFunction;
