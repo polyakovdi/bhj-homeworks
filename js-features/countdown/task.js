42cca1cffbc503242a6b7b6c8755517dd96d4590
polyakovdi <goodboy393393@gmail.com>
2023-05-02 18:51:47 +0300

    Первое задание выполнено

---
+++ js-features/countdown/task.js
@@ -0,0 +1,9 @@
+const ReverseTimer = function () {
+    let output = document.getElementById("timer");
+    output.textContent -= 1;
+    if (Number(output.textContent) === 0) {
+        alert('Вы победили в конкурсе');
+        clearInterval(inervalId);
+    }
+}
+const inervalId = setInterval(ReverseTimer, 1000);
