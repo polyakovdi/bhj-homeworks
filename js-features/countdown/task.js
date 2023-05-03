8dedfdeb5e18c773fab154f12b2410eab722912f
polyakovdi <goodboy393393@gmail.com>
2023-05-03 10:02:11 +0300

    First homework done!

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,11 +1,11 @@
 // Задание 1
-let countDownn = function() {
-    let timer = document.getElementById("timer");
-    timer.textContent--;
-    if (timer.textContent <= 0) {
-      clearInterval(timerId);
-      alert("Вы победили в конкурсе!");
-    } 
-}
-
-let timerId = setInterval(countDownn, 1000);
+const timer = document.getElementById('timer');
+const interval = setInterval(() => {
+   if (timer.textContent == 0) {
+      alert('Вы победили в конкурсе!');
+      clearInterval(interval);
+   }
+   if (timer.textContent > 0) {
+      timer.textContent--;
+   }
+}, 1000);
