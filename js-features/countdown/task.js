c9aed6847b14c86a60b643800f082d3175cbaedf
polyakovdi <goodboy393393@gmail.com>
2023-05-03 09:45:16 +0300

    First homework done!

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -6,7 +6,7 @@ function countDown() {
     document.getElementById('timer').innerHTML = x;
     x--;
 
-    if (x < 0) {
+    if (x <= 0) {
         clearTimeout (timer);
         alert("Вы победили в конкурсе!")
     } else {
